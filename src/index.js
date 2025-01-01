f07fc97b112bf9e6437c5e92e4c83f72f943be23
Harsh Sharma <124427013+harshh-sharma@users.noreply.github.com>
2025-01-01 15:28:14 +0530

    adding info controller to check server live or not

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const { PORT } = require("./config");
+const apiRouter = require("./routes");
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use('/api',apiRouter);
 
 
 app.listen(PORT,() => {
